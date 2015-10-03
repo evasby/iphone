27e176ee1ed0781e2b36faf86c86859dff4330d8
Vladimir Dmitriev <defurator@mail.ru>
2015-10-03 20:08:12 +0300

    header + main1 begin

--- index.php
+++ index.php
@@ -24,10 +24,65 @@
 </head>
 <body>
   <header class="header">
-    
+    <div class="header_top">
+      <div class="myinner">
+        <div class="phones">
+          <p>+375 (29) 996-89-89</p>
+          <p>+375 (29) 769-97-69</p>
+          <p>+375 (17) 294-49-00</p>
+        </div>
+        <div class="address">
+          <p>Минск, Платонова ул. 43, офис 115</p>
+        </div>
+        <div class="social">
+          <a href="#" class="social_link social_link__vk">Вконтакте</a>
+          <a href="#" class="social_link social_link__fb">Facebook</a>
+        </div>
+        <div class="menu-apple">
+          <ul class="menu-apple_list">
+            <li class="menu-apple_item">
+              <a href="#" class="menu-apple_link">Sales</a>
+            </li>
+            <li class="menu-apple_item">
+              <a href="#" class="menu-apple_link">Service</a>
+            </li>
+            <li class="menu-apple_item">
+              <a href="#" class="menu-apple_link">Support</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="header_bottom">
+      <div class="myinner">
+        <nav class="menu-top">
+          <ul class="menu-top_list">
+            <li class="menu-top_item">
+              <a href="#" class="menu-top_link">Услуги и цены</a>
+            </li>
+            <li class="menu-top_item">
+              <a href="#" class="menu-top_link">О нас</a>
+            </li>
+            <li class="menu-top_item">
+              <a href="#" class="menu-top_link">Отзывы</a>
+            </li>
+            <li class="menu-top_item">
+              <a href="#" class="menu-top_link">Акции</a>
+            </li>
+            <li class="menu-top_item">
+              <a href="#" class="menu-top_link">Новости</a>
+            </li>
+            <li class="menu-top_item">
+              <a href="#" class="menu-top_link">Контакты</a>
+            </li>
+          </ul>
+        </nav>
+        <a href="/layout/" class="logo">i-World</a>
+      </div>
+    </div>
   </header>
 
-    
+  <section class="main1"></section>
 
   <footer class="footer">
     
